26b12bea0bad677d65ed35414c16be71376e4329
matt-balda <mateusbalda89@gmail.com>
2023-01-20 00:48:25 -0300

    Construtor classe SubstituicaoPagina e método Simular

--- src/SubstituicaoPagina.java
+++ src/SubstituicaoPagina.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class SubstituicaoPagina {
@@ -11,4 +12,15 @@ public class SubstituicaoPagina {
 
     // Estrutura para armazenar as páginas presentes na memória
     private Queue<Pagina> memoria;
+
+    // Construtor
+    public SubstituicaoPagina(int tamanhoMemoria) {
+        this.tamanhoMemoria = tamanhoMemoria;
+        memoria = new LinkedList<Pagina>();
+    }
+
+    // Método para ler o arquivo de trace e simular a alocação de memória
+    public void simular(String nomeArquivo, String algoritmo) {
+
+    }
 }
