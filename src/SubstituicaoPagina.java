98369d9f010e077bdc0c6684d9b3bc96b1aa6e96
matt-balda <mateusbalda89@gmail.com>
2023-01-20 00:40:10 -0300

    Adição dos atributos para a classe SubstituicaoPafina

---
+++ src/SubstituicaoPagina.java
@@ -0,0 +1,12 @@
+package src;
+
+public class SubstituicaoPagina {
+    // Tamanho da memória em frames
+    private int tamanhoMemoria;
+
+    // Tamanho de cada página em bytes
+    private final int TAMANHO_PAGINA = 4096;
+
+    // Estrutura para armazenar as páginas presentes na memória
+    private Queue<Page> memoria;
+}
